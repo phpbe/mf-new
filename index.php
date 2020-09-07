2dc84b09019772d80054cfde52391889095ce903
刘一二 <liuguoqiang@haitun.hk>
2020-09-07 10:49:56 +0800

    初始化

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$loader = require(dirname(__DIR__) . '/vendor/autoload.php');
+$loader->addPsr4('Be\\', __DIR__);
+
+$runtime = \Be\System\Be::getRuntime();
+$runtime->setRootPath(__DIR__);
+$runtime->execute();
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+#### 简介
+
+PHP BE 管理框架（Management Framework）
+
+
+#### 安装
+
+    composer create be/mf-new
